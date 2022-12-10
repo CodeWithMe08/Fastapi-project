13f2fdc7593aae9e0cc09d386725a9b62e8493da
Raunak Varma <raunakvarma05@gmail.com>
2022-12-10 20:59:21 +0530

    added pydentic

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    contetn: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -14,8 +21,11 @@ def get_posts():
     return {"data":"posts"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post":f"title {payload['title']} content {payload['contetn']}"}
+def create_posts(new_post: Post):
+    print(new_post)
+    return {"data":"new_post"}
+
+
+
 
 
