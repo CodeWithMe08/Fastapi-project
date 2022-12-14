e8f08ea00e8fb502913fac04cd9e1c053df89ac2
Raunak Varma <raunakvarma05@gmail.com>
2022-12-14 23:56:58 +0530

    added authorized_client function and creating, mapping, storingtest_posts

---
+++ tests/test_posts.py
@@ -0,0 +1,8 @@
+import pytest
+from app import schemas
+
+
+def test_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts/")
+    print(res.json())
+    assert res.status_code == 200
